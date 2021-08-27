7933b348ebf46f90f0b3f83602855e6e1ef20576
OjasMalani <79085373+OjasMalani@users.noreply.github.com>
2021-08-27 15:49:33 +0530

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -35,3 +35,7 @@ function redirectToRoomName(name) {
   localStorage.setItem("room name",name);
   window.location="kwitter_page.html";
 }
+
+ function logout() {
+  localStorage.removeItem("user_name"); localStorage.removeItem("room_name"); window.location = "kwitter.html";
+ }
